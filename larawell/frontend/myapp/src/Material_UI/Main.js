479392f5ca19cd2c243cbbcb91ff57202f3cced2
abhishek <abhishekshenoy7@gmail.com>
2022-01-10 11:35:03 +0530

    second commit mergeg from auth branch

--- larawell/frontend/myapp/src/Material_UI/Main.js
+++ larawell/frontend/myapp/src/Material_UI/Main.js
@@ -2,34 +2,48 @@ import React from  'react';
 import {Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container }  from '@material-ui/core';
 import AppsIcon from '@material-ui/icons/Apps'; 
 import BarChart from '../charts/BarChart';
+import ReactSpeedometer from "react-d3-speedometer";
+
 
 const Main =  () => {   
+    const textColor = '#AAA';
     return(
         <>
-            <CssBaseline />
-            <AppBar position='relative'>
-                <Toolbar>
-                    <AppsIcon />
-                    <Typography variant="h6">
-                        Shopper stop
-                    </Typography>
-                    
-                </Toolbar>                
-            </AppBar>
-            <main>
-                <div>
-                    <Container maxWidth="sm">
-                        <Typography variant="h2" align="center" color="textSecondary" gutterBottom>
-                           Graph data
-                        </Typography>
-                        <Typography variant="h6" align="center" color="textPrimary" paragraph>
+        <CssBaseline />
+        <AppBar position='relative'>
+            <Toolbar>
+                <AppsIcon />
+                <Typography variant="h6">
+                    Shopper stop
+                </Typography>
                 
-                        </Typography>
-                        <BarChart />    
+            </Toolbar>                
+        </AppBar>
+        <main>
+            <div>
+                <Container maxWidth="sm">
+                    <Typography variant="h2" align="center" color="textSecondary" gutterBottom>
+                        Graph data
+                    </Typography>
+                    <Typography variant="h6" align="center" color="textPrimary" paragraph>
+            
+                    </Typography>
+                    <BarChart />                      
+                    <ReactSpeedometer
+                        maxValue={100}
+                        value={473}
+                        needleColor="red"
+                        startColor="green"
+                        segments={10}
+                        endColor="blue"
+                        textColor="grey"
+                        textColor={textColor}
+                        width={200}
+                        height={200}
+                    />                    
                     </Container>
-                </div>
-            </main>
-
+            </div>
+        </main>
         </>
     );
 };
